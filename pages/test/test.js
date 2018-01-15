2ba867bdc2f2b23b339183f763139ab455a6b4e1
cc <19948031@qq.com>
2018-01-15 22:52:44 +0800

    编写测试页面做测试

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,12 +1,13 @@
 // pages/test/test.js
-let shareOptions = { "from": "menu" }
+
+const AV = require('../../utils/av-weapp-min');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    scanCodeBase64:''
   },
   /**
    * 生命周期函数--监听页面加载
@@ -15,30 +16,12 @@ Page({
 
   },
   share() {
+    let paramsJson = {};
     let that = this;
-    var Config = new wx.BaaS.TableObject(21425);
-    Config.find().then(res => {
-      console.log(res.data.objects[0])
-      getAccessToken(res.data.objects[0].appid, res.data.objects[0].appsecret)
+    AV.Cloud.run('getScanCode', paramsJson).then(res => {
+      console.log(res);
+      that.setData({ scanCodeBase64: res })
     }).catch(err => console.log(err))
-    function getAccessToken(APPID, APPSECRET) {
-      wx.BaaS.request({
-        url: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${APPSECRET}`
-      }).then(res => {
-        console.log(res)
-        return wx.BaaS.request({
-          // url: `https://api.weixin.qq.com/wxa/getwxacode?access_token=${res.data.access_token}`,
-          url: `https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=${res.data.access_token}`,
-          method: 'POST',
-          data: {
-            "scene": "test",
-            "auto_color": true
-          }
-        })
-      }).then(res => {
-        console.log(res)
-       that.setData({imgData:wx.arrayBufferToBase64(res.data)})
-      }).catch(err => console.log(err))
-    }
+
   }
 })
