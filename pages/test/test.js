6c8168392e4d7541ac9cc4f67a9ae3a7005e1e48
cc <19948031@qq.com>
2018-01-16 17:29:41 +0800

    canvas初步测试

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,13 +1,14 @@
 // pages/test/test.js
 
 const AV = require('../../utils/av-weapp-min');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    scanCodeBase64:''
+    codeUrl: ''
   },
   /**
    * 生命周期函数--监听页面加载
@@ -16,11 +17,14 @@ Page({
 
   },
   share() {
-    let paramsJson = {};
+    let paramsJson = { details: 1 };
     let that = this;
     AV.Cloud.run('getScanCode', paramsJson).then(res => {
       console.log(res);
-      that.setData({ scanCodeBase64: res })
+      that.setData({ codeUrl: res.url })
+      let ctx = wx.createCanvasContext('myCanvas')
+      ctx.drawImage(res.url, 0, 0, 150, 150)
+      ctx.draw()
     }).catch(err => console.log(err))
 
   }
