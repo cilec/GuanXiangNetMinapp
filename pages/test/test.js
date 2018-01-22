828cc7698d937101df5b5d3a70e47443c2b681cf
cc <19948031@qq.com>
2018-01-22 21:31:19 +0800

    测试promise

--- pages/test/test.js
+++ pages/test/test.js
@@ -13,45 +13,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-  share() {
-    let paramsJson = {};
-    let that = this;
-    AV.Cloud.run('getScanCode', paramsJson).then(res => {
-      console.log(res);
-      let QRcodeUrl = res.url;
-      drawShareImage('12.1足球竞猜数据', '28元', QRcodeUrl)
-    }).catch(err => console.log(err))
-
+    new Promise((resolve, reject) => {
+      wx.authorize({
+        scope: 'scope.userInfo',
+        success({ errMsg }) {
+          if (errMsg == "authorize:fail auth deny") {
+            reject()
+          } else {
+            resolve()
+          }
+        },
+        fail() {
+          reject()
+        }
+      })
+    }).then((res) => {
+      console.log('success')
+    }, wx.showModal({
+      title: '提示',
+      content: '很抱歉，访问需要授权！如何重新授权：删除小程序后重新进入',
+      showCancel: false,
+      confirmText: '好的',
+      confirmColor: '#FD544A',
+      complete() {
+        wx.reLaunch({
+          url: "../DayRecommended/DayRecommended",
+        })
+      }
+    }))
   }
 })
-function drawShareImage(title, price, qrCodeUrl) {
-  const ctx = wx.createCanvasContext('myCanvas');
-  let { windowWidth } = wx.getSystemInfoSync();
-  let radio = windowWidth / 750;
-  let { avatarUrl } = wx.BaaS.storage.get('userinfo')
-  //画头像
-  ctx.drawImage(avatarUrl, windowWidth / 2 - 40, 10, 80, 80);
-  //把头像画成圆形
-  ctx.setLineWidth(20);
-  ctx.setStrokeStyle('white')
-  //线条宽度为20，那么半径就等于线条宽度一半加图片宽度一半
-  ctx.arc(windowWidth / 2, 50, 50, 0, 2 * Math.PI)
-  ctx.stroke()
-  //画标题
-  ctx.setFillStyle('red')
-  ctx.fillRect(windowWidth / 2 - windowWidth * 0.7 / 2, 110, windowWidth * 0.7, 14 / radio)
-  ctx.setFontSize(10 / radio)
-  ctx.setTextBaseline('middle')
-  ctx.setTextAlign('center')
-  ctx.setFillStyle('white')
-  ctx.fillText(title, windowWidth / 2, 110 + 7 / radio)
-  //画价格
-  ctx.setFillStyle('red');
-  ctx.setFontSize(18 / radio);
-  ctx.fillText(price, windowWidth / 2 - windowWidth * 0.7 / 4, 250)
-  //画二维码
-  ctx.drawImage(qrCodeUrl, windowWidth / 2, 250 - 75, 150, 150)
-  ctx.draw()
-}
+
