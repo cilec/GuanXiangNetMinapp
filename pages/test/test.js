3a321feae55c6fe472be9e482a65822986f0559d
cc <19948031@qq.com>
2018-01-23 17:07:07 +0800

    review代码,修改验证逻辑

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,46 +1,48 @@
 // pages/test/test.js
 
-const AV = require('../../utils/av-weapp-min');
+const AV = require("../../utils/av-weapp-min");
 Page({
-
   /**
    * 页面的初始数据
    */
-  data: {
-    scanCodeBase64: ''
-  },
+  data: {},
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    new Promise((resolve, reject) => {
-      wx.authorize({
-        scope: 'scope.userInfo',
-        success({ errMsg }) {
-          if (errMsg == "authorize:fail auth deny") {
-            reject()
-          } else {
-            resolve()
-          }
-        },
-        fail() {
-          reject()
-        }
-      })
-    }).then((res) => {
-      console.log('success')
-    }, wx.showModal({
-      title: '提示',
-      content: '很抱歉，访问需要授权！如何重新授权：删除小程序后重新进入',
+  onLoad: function(options) {
+    checkAuthorize().then(res=>{
+      if(!res){
+
+      }
+    })
+    wx.showModal({
+      title: "提示",
+      content: "很抱歉，访问需要授权！请删除小程序后重新进入",
       showCancel: false,
-      confirmText: '好的',
-      confirmColor: '#FD544A',
+      confirmText: "好的",
+      confirmColor: "#FD544A",
       complete() {
         wx.reLaunch({
-          url: "../DayRecommended/DayRecommended",
-        })
+          url: "../DayRecommended/DayRecommended"
+        });
       }
-    }))
+    });
   }
-})
-
+});
+function checkAuthorize() {
+  return new Promise((resolve, reject) => {
+    wx.authorize({
+      scope: "scope.userInfo",
+      success({ errMsg }) {
+        if (errMsg == "authorize:ok") {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      },
+      fail() {
+        resolve(false);
+      }
+    });
+  });
+}
